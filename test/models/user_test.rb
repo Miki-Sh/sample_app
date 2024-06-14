a0cb4107fdcfa910a5e9d7bc69ab04e999abb1e4
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-14 02:25:17 +0000

    一部のデータベースアダプタが、大文字小文字を区別するインデックスを常に使っているとは限らない問題への対処ため、データベースに保存される直前にすべての文字列を小文字に変換するよう変更（データベースレベルでもemailの一意性を強制する②）

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,7 +56,6 @@ class UserTest < ActiveSupport::TestCase
   # 重複するメールアドレス拒否のテスト
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
