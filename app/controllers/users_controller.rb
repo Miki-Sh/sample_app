2d9ccb82009e5dc83ec35acdec377f30d9e5fdf3
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-14 14:00:37 +0000

    ユーザー登録の成功/失敗に対応したcreateアクションを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
 end
