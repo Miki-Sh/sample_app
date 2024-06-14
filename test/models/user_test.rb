c1dcb7b8b968e13f3f2ded57ad18cba8e2a86b8b
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-14 02:40:33 +0000

    メールアドレスを小文字で保存できているかのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,4 +59,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+  # メールアドレスを小文字で保存できているかのテスト
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
