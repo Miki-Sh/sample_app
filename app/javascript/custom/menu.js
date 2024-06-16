1273963cbc95268b40335c4bc4f02fcbfa544c43
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-16 16:54:58 +0000

    モバイルモードでハンバーガーメニューを使うようにする

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,13 +1,17 @@
 // メニュー操作
 
-// トグルリスナーを追加してクリックをリッスンする
+// トグルリスナーを追加する
+const addToggleListener = (selecter_id, menu_id, toggle_class) => {
+  let selected_element = document.querySelector(`#${selecter_id}`);
+  selected_element.addEventListener("click", (e) => {
+    e.preventDefault();
+    let menu = document.querySelector(`#${menu_id}`);
+    menu.classList.toggle(toggle_class);
+  });
+};
+
+// クリックをリッスンするトグルリスナーを追加する
 document.addEventListener("turbo:load", () => {
-  let account = document.querySelector("#account");
-  if (account) {
-    account.addEventListener("click", (event) => {
-      event.preventDefault();
-      let menu = document.querySelector("#dropdown-menu");
-      menu.classList.toggle("active");
-    });
-  }
+  addToggleListener("hamburger", "navbar-menu",   "collapse");
+  addToggleListener("account",   "dropdown-menu", "active");  
 });
