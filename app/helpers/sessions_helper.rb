4cb211fe4741419637c8883306f94db4ab841be8
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-16 14:57:40 +0000

    current_userメソッドを定義して、セッションIDに対応するユーザー名をデータベースから取り出せるようにする

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（セッションにユーザーIDが存在する場合のみ）
+  def current_user
+    if session[:user_id]
+      # 1回のリクエスト内におけるデータベースへの問い合わせが最初の1回だけで済むようUser.find_byの実行結果をインスタンス変数に保存して、次回から再利用
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
