5546eb99475cced562582bb5b10c8fb4b6736ee8
Miki-Sh <128499788+Miki-Sh@users.noreply.github.com>
2024-06-16 01:08:16 +0000

    有効な値の送信をログインフォームで正しく扱うため、ユーザーIDを一時セッションの中に安全に置けるようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      # セッション固定攻撃（攻撃者が既に持っているセッションidをユーザーに使わせるように仕向けることで、攻撃者がユーザーと意図的にセッションを共有して情報を奪う手法）への対策として、ユーザーがログインする直前にセッションを必ず即座にリセットする
+      reset_session   # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
