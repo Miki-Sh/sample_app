2eecbcb5564134b3ac1ba2152d05b7bf25f8e62a
Miki-Sh <srnin23@gmail.com>
2024-06-18 05:01:46 +0000

    cookiesメソッドを使い、ユーザーの暗号化済みIDと記憶トークンをブラウザの永続cookiesに保存して、永続的なセッションを作成する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # セッション固定攻撃（攻撃者が既に持っているセッションidをユーザーに使わせるように仕向けることで、攻撃者がユーザーと意図的にセッションを共有して情報を奪う手法）への対策として、ユーザーがログインする直前にセッションを必ず即座にリセットする
       reset_session   # ログインの直前に必ずこれを書くこと
+      remember user
       log_in user
       redirect_to user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す（セッションにユーザーIDが存在する場合のみ）
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 現在ログイン中のユーザーを返す（sessionまたはcookiesにユーザーIDが存在する場合のみ）
   def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       # 1回のリクエスト内におけるデータベースへの問い合わせが最初の1回だけで済むようUser.find_byの実行結果をインスタンス変数に保存して、次回から再利用
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    # 記憶トークンcookieに対応するユーザーを返す
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
