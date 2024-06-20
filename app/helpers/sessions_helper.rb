7ba8e76b039cc07041304606f75595cc6116ffbe
Miki-Sh <srnin23@gmail.com>
2024-06-20 04:23:13 +0000

    セッション内にあるuser_idの、セッションリプレイ攻撃に対する脆弱さを修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,8 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する。詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+    session[:session_token] = user.session_token
   end
 
   # 永続的セッションのためにユーザーをデータベースに記憶する
@@ -15,8 +17,10 @@ module SessionsHelper
   # 現在ログイン中のユーザーを返す（sessionまたはcookiesにユーザーIDが存在する場合のみ）
   def current_user
     if (user_id = session[:user_id])
-      # 1回のリクエスト内におけるデータベースへの問い合わせが最初の1回だけで済むようUser.find_byの実行結果をインスタンス変数に保存して、次回から再利用
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     # 記憶トークンcookieに対応するユーザーを返す
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,13 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest # update_attributeの結果を返すのではなく、ダイジェストを返すようにする
+  end
+
+  # セッションハイジャック防止のためにセッショントークンを返す
+  # この記憶ダイジェストを再利用しているのは単に利便性のため
+  def session_token
+    remember_digest || remember # remember_digestは既に一意の値になっており、各ユーザーと紐付けられているので、単にこれをセッショントークンとして再利用し、ユーザーがまだ存在しない場合は生成する
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
