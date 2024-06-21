70ee51019a007c7c30259e369dc2a8a9f40c0bf2
Miki-Sh <srnin23@gmail.com>
2024-06-21 02:03:03 +0000

    ユーザー編集成功時のテストと処理を作成

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,5 +11,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
     assert_template 'users/edit'
+    assert_select 'div.alert', count: 1
+  end
+
+  test "ユーザー編集成功" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
   end
 end
