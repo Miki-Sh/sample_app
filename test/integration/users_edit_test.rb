dba55b6cee29014fe94957d884108c6113fcea2c
Miki-Sh <srnin23@gmail.com>
2024-06-21 05:20:40 +0000

    test名を若干修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -30,13 +30,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
-  test "ログインしていない場合、editにリダイレクトする" do
+  test "editアクション時ログインしていない場合、flashメッセージを出してログインページにリダイレクトする" do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
-  test "ログインしていない場合、updateにリダイレクトする" do
+  test "updateアクション時ログインしていない場合、flashメッセージを出してログインページにリダイレクトする" do
     patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
