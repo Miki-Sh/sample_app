5ad18501595e3918bf5936c878438bd4470c574f
Miki-Sh <srnin23@gmail.com>
2024-06-27 16:00:50 +0000

    ユーザー有効化コードをリファクタリング

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,8 +3,7 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
      # !user.activated?　：既に有効になっているユーザーを誤って再度有効化しない　→　攻撃者がユーザーの有効化リンクを後から盗みだしてクリックし、本当のユーザーとしてログインするのを防ぐ
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.activate
       log_in user
       flash[:success] = "アカウントが有効化されました!"
       redirect_to user
