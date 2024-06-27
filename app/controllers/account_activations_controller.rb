56f027a1ed9ec5dbd50503a9ec2c3b3bdccdbb56
Miki-Sh <srnin23@gmail.com>
2024-06-27 03:06:44 +0000

    editアクションを有効化

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+     # !user.activated?　：既に有効になっているユーザーを誤って再度有効化しない　→　攻撃者がユーザーの有効化リンクを後から盗みだしてクリックし、本当のユーザーとしてログインするのを防ぐ
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "アカウントが有効化されました!"
+      redirect_to user
+    else
+      flash[:danger] = "無効な有効化リンクです"
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,19 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      # セッション固定攻撃（攻撃者が既に持っているセッションidをユーザーに使わせるように仕向けることで、攻撃者がユーザーと意図的にセッションを共有して情報を奪う手法）への対策として、ユーザーがログインする直前にセッションを必ず即座にリセットする
-      reset_session   # ログインの直前に必ずこれを書くこと
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      log_in @user
-      redirect_to forwarding_url || @user
+      if @user.activated?
+        forwarding_url = session[:forwarding_url]
+        # セッション固定攻撃（攻撃者が既に持っているセッションidをユーザーに使わせるように仕向けることで、攻撃者がユーザーと意図的にセッションを共有して情報を奪う手法）への対策として、ユーザーがログインする直前にセッションを必ず即座にリセットする
+        reset_session   # ログインの直前に必ずこれを書くこと
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
+      else
+        message  = "アカウントが有効化されていません。 "
+        message += "メールを確認して、有効化リンクをクリックしてください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "無効なemail/passwordの組み合わせです"
       render 'new', status: :unprocessable_entity
