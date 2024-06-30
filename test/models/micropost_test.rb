6001086bc435a5ce2809ecf9a544938a7f913385
Miki-Sh <srnin23@gmail.com>
2024-06-30 16:00:13 +0000

    Micropostのバリデーションとテストを作成

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "micropostは有効である" do
+    assert @micropost.valid?
+  end
+
+  test "ユーザーIDは存在する" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "contentは存在する" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "contentは140文字以内である" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
