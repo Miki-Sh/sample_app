ebab35a9e7147182d14032c7a79e1a6c541221b8
Miki-Sh <srnin23@gmail.com>
2024-07-02 06:38:07 +0000

    UserモデルとMicropostモデルを関連づける

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to :user    # マイクロポストがユーザーに所属する（belongs_to）関連付け
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
