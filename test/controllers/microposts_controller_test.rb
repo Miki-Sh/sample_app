a0dd88674751ed2a71f1dffd15ca066f510a0fc9
Miki-Sh <srnin23@gmail.com>
2024-07-03 08:25:54 +0000

    ログイン済みユーザーのみ、createアクションやdestroyアクションを実行する

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "ログインしていない時、createアクションを実行するとログインURLへリダイレクトする" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "ログインしていない時、destroyアクションを実行するとログインURLへリダイレクトする" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
 end
