2e16ef02ce1baeedca33c638eba2e2f1a56c642d
Miki-Sh <srnin23@gmail.com>
2024-07-09 04:56:35 +0000

    🦺 Relationshipモデルのバリデーションを追加

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id,
+                                     followed_id: users(:archer).id)
+  end
+
+  test "@relationshipは有効か" do
+    assert @relationship.valid?
+  end
+
+  test "follower_idを要求している" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "followed_idを要求している" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
