9ed3fbafe444c2e53f02b087081f465f7427fa13
Miki-Sh <srnin23@gmail.com>
2024-07-10 15:10:46 +0000

    🔒️ ✨ ✅ Relationshipsコントローラのアクションと、それにアクセスするとき、ログイン済みのユーザーであるかどうかをチェックするテストを作成

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "ログインせずにcreateアクションを実行するとログインページにリダイレクトする" do
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
+  test "ログインせずにdestroyアクションを実行するとログインページにリダイレクトする" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
