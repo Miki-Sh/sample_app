816b699e2ff0fd80b523b7fafe65c00806946e18
Miki-Sh <srnin23@gmail.com>
2024-07-12 15:34:50 +0000

    ✅ ［Follow］/［Unfollow］ボタンのテストを作成

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Following < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 end
@@ -25,6 +26,45 @@ class FollowPagesTest < Following
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert 'a[href=?]', user_path(user)
+    end
+  end
+end
+
+class FollowTest < Following
+  test "標準のフォロー" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+    assert_redirected_to @other
+  end
+
+  test "Hotwireを使ったフォロー" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path(format: :turbo_stream), params: { followed_id: @other.id }
+    end
+  end
+end
+
+class Unfollow < Following
+  def setup
+    super
+    @user.follow(@other)
+    @relationship = @user.active_relationships.find_by(followed_id: @other.id)
+  end
+end
+
+class UnfollowTest < Unfollow
+  test "標準のアンフォロー" do
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(@relationship)
+    end
+    assert_response :see_other
+    assert_redirected_to @other
+  end
+
+  test "Hotwireを使ったアンフォロー" do
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(@relationship, format: :turbo_stream)
     end
   end
 end
