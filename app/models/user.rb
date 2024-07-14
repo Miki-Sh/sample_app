91df49e7e932ecb0de54a2dc4ff556f8087184ff
Miki-Sh <srnin23@gmail.com>
2024-07-14 16:30:09 +0000

    ⚡️ N+1クエリ問題を解決するため、eager loadingを使用しマイクロポストを取り出す1件のクエリの中にユーザーおよび添付画像を取り出すクエリも含めるよう変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,7 +81,7 @@ class User < ApplicationRecord
   # ユーザーのステータスフィードを返す
   def feed
     following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
   end
   
   # ユーザーをフォローする
